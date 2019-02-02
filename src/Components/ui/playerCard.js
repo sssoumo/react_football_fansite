998b2957748554f96e0238fac425bb3fa5678884
Saadman <sssoumo@gmail.com>
2019-02-03 01:16:23 +0600

    change in player card

--- src/Components/ui/playerCard.js
+++ src/Components/ui/playerCard.js
@@ -4,13 +4,12 @@ const PlayerCard = (props) => {
     return (
         <div className="player_card_wrapper">
             <div 
-            className="player_card_thmb"
+                className="player_card_thmb"
                 style={{background:`#f2f9ff url(${props.bck})`}}
-            >
-                <div className="player_card_nfo">
-                    <div className="player_card_number">
-                        {props.number}
-                    </div>
+            ></div>
+            <div className="player_card_nfo">
+                <div className="player_card_number">
+                    {props.number}
                 </div>
                 <div className="player_card_name">
                     <span>{props.name}</span>
